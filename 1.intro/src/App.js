55e760e5267ca526721cf3a817e59f461044dd7d
kronosGR <geo.elgeo@gmail.com>
2022-07-08 16:10:49 +0200

    use of props

--- 1.intro/src/App.js
+++ 1.intro/src/App.js
@@ -1,51 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-const list = [
+const stories = [
   {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
     num_comments: 3,
     points: 4,
     objectID: 0,
   },
   {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
     num_comments: 2,
     points: 5,
     objectID: 1,
   },
 ];
 
-const welcome={
-  greeting:'Hey',
-  title:'React'
-}
+const welcome = {
+  greeting: "Hey",
+  title: "React",
+};
 function App() {
-  
   return (
-   <div>
-    <h1>Hello {welcome.greeting} {welcome.title}</h1>
-    <label htmlFor='search'>Search: </label>
-    <input id='search' type='text'/>
+    <div>
+      <h1>
+        Hello {welcome.greeting} {welcome.title}
+      </h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
 
-    <hr/>
-    {list.map(item => {
-      return (
-      <div key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-        </div>)
-    })}
-   </div>
+      <hr />
+      <List list={stories} />
+    </div>
   );
 }
+
+const List = (props) =>
+  props.list.map((item) => (
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
+  ));
 
 export default App;
