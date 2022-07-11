8c14a5a7f791e69b1dd66095df502ce38ee93333
kronosGR <geo.elgeo@gmail.com>
2022-07-11 17:12:25 +0200

    changes to support dev/prod

--- 2.react_and_firebase_authentictaion/src/components/Firebase/firebase.js
+++ 2.react_and_firebase_authentictaion/src/components/Firebase/firebase.js
@@ -1,7 +1,7 @@
 import firebase from "firebase/compat";
 import app from "firebase.app";
 
-const config = {
+const devConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
@@ -9,6 +9,17 @@ const config = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
+
+const prodConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+};
+
+const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
 
 class Firebase {
   constructor() {
