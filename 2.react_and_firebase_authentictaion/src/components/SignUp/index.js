a242e82e47ffa64bad5f622069aff6712f5734d7
kronosGR <geo.elgeo@gmail.com>
2022-07-12 16:59:19 +0200

    implemented sign up form

--- 2.react_and_firebase_authentictaion/src/components/SignUp/index.js
+++ 2.react_and_firebase_authentictaion/src/components/SignUp/index.js
@@ -1,9 +1,75 @@
-import React from "react";
+import React, { Component, useState } from "react";
+
+import * as ROUTES from "../../constants/routes";
+import { Link } from "react-router-dom";
 
 const SignUpPage = () => (
   <div>
-    <h1>SignUpPage</h1>
+    <h1>SignUp</h1>
+    <SignUpForm />
   </div>
 );
 
+const INITIAL_STATE = {
+  username: "",
+  email: "",
+  passwordOne: "",
+  passwordTwo: "",
+  error: null,
+};
+
+const SignUpForm = (props) => {
+  const [state, setState] = useState(INITIAL_STATE);
+  const onSubmit = (event) => {};
+
+  const onChange = (event) => {
+    setState({ [event.target.name]: event.target.value });
+  };
+
+  const { username, email, passwordOne, passwordTwo, error } = state;
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        name="username"
+        onChange={onChange}
+        value={username}
+        type="text"
+        placeholder="Full Name"
+      />
+      <input
+        name="email"
+        value={email}
+        onChange={onChange}
+        type="text"
+        placeholder="Email Address"
+      />
+      <input
+        name="passwordOne"
+        value={passwordOne}
+        onChange={onChange}
+        type="password"
+        placeholder="Password"
+      />
+
+      <input
+        name="passwordTwo"
+        value={passwordTwo}
+        onChange={onChange}
+        type="password"
+        placeholder="Confirm Password"
+      />
+      <button type="submit">Sign up</button>
+
+      {error && <p>{error.message}</p>}
+    </form>
+  );
+};
+const SignUpLink = () => (
+  <p>
+    Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+  </p>
+);
+
 export default SignUpPage;
+
+export { SignUpForm, SignUpLink };
