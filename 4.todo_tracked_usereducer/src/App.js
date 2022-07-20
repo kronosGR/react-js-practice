db56edc3185ed24aca0438261535245860e1c3be
kronosGR <geo.elgeo@gmail.com>
2022-07-20 16:52:06 +0200

    added reducer

--- 4.todo_tracked_usereducer/src/App.js
+++ 4.todo_tracked_usereducer/src/App.js
@@ -1,5 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const initialState = {
+  todos: [
+    { id: 1, title: "Wash dishes" },
+    { id: 2, title: "Study JS" },
+    { id: 3, title: "Buy ticket" },
+  ],
+  query: "",
+};
+
+let nextId = 4;
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return {
+        ...state,
+        todos: [...state.todos, { id: nextId++, title: action.title }],
+      };
+    case "DELETE_TODO":
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.id),
+      };
+    case "TOGGLE_TODO":
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
+        ),
+      };
+    case "SET_QUERY":
+      return {
+        ...state,
+        query: action.query,
+      };
+    default:
+      return state;
+  }
+};
 
 function App() {
   return (
