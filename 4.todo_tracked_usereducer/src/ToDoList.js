bb786da9aeceb7abf4b592648edadecc4145b1a4
kronosGR <geo.elgeo@gmail.com>
2022-07-21 16:26:29 +0200

    added ToDoList component

---
+++ 4.todo_tracked_usereducer/src/ToDoList.js
@@ -0,0 +1,24 @@
+import {useDispatch, useTrackedState} from "./store";
+
+const ToDoList = ()=>{
+  const dispatch = useDispatch()
+  const state = useTrackedState()
+  const setQuery = event =>{
+    dispath({type:'SET_QUERY', query: event.target.value})
+  }
+
+  return(
+    <div>
+      <ul>
+        {state.todos.map(({id, title, completed}) => (
+          //  <TodoItem key={id} id={id} title={title} completed={completed} />
+        ))}
+          // <NewTodo />
+      </ul>
+      <div>
+        Highlight Query for incomplete items:
+        <input vale={state.query} onChange={setQuery}/>
+      </div>
+    </div>
+  )
+}
