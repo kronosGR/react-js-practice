78c6cba79d2a0628d03125271cf99c5cd217f0dd
kronosGR <geo.elgeo@gmail.com>
2022-07-28 16:47:28 +0200

    small refactoring

--- 6. learn_react/src/App.js
+++ 6. learn_react/src/App.js
@@ -44,7 +44,12 @@ function App() {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search onSearch={handleSearch} search={searchTerm} />
+      <InputWithLabel
+        id="search"
+        label="Search"
+        value={searchTerm}
+        onInputChange={handleSearch()}
+      />
       <hr />
 
       <List list={searchedStories} />
@@ -52,12 +57,13 @@ function App() {
   );
 }
 
-const Search = ({ search, onSearch }) => {
+const InputWithLabel = ({ id, label, value, type = "text", onInputChange }) => {
   return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" value={search} onChange={onSearch} />
-    </div>
+    <>
+      <label htmlFor={id}>{label}</label>
+      &nbsp;
+      <input id={id} type={type} value={value} onChange={onInputChange} />
+    </>
   );
 };
 
