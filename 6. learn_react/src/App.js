f3df7f73453ac3ece13b9c47356cbc758d2daed1
kronosGR <geo.elgeo@gmail.com>
2022-07-28 17:04:26 +0200

    added children to component

--- 6. learn_react/src/App.js
+++ 6. learn_react/src/App.js
@@ -46,10 +46,11 @@ function App() {
 
       <InputWithLabel
         id="search"
-        label="Search"
         value={searchTerm}
         onInputChange={handleSearch()}
-      />
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
       <hr />
 
       <List list={searchedStories} />
@@ -57,10 +58,16 @@ function App() {
   );
 }
 
-const InputWithLabel = ({ id, label, value, type = "text", onInputChange }) => {
+const InputWithLabel = ({
+  id,
+  value,
+  type = "text",
+  onInputChange,
+  children,
+}) => {
   return (
     <>
-      <label htmlFor={id}>{label}</label>
+      <label htmlFor={id}>{children}</label>
       &nbsp;
       <input id={id} type={type} value={value} onChange={onInputChange} />
     </>
