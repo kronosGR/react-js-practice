6a991073fb403d9afc396c19f77241b9bee8c444
kronosGR <geo.elgeo@gmail.com>
2022-08-04 15:42:07 +0200

    added simple reducer and sample component

--- 7. redux_a/src/App.js
+++ 7. redux_a/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { createStore } from "redux";
+import reducer from "./reducers";
+import Hello from "./Hello";
+
+const initialState = { tech: "React" };
+
+const store = createStore(reducer, initialState);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hello tect={store.getState()} />
     </div>
   );
 }
---
+++ 7. redux_a/src/Hello.js
@@ -0,0 +1,3 @@
+export default function Hello({ tech }) {
+  return <div>Hello {tech}</div>;
+}
