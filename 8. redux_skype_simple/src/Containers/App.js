9366ff82e0e54cde0a5091544147af8af531ed02
kronosGR <geo.elgeo@gmail.com>
2022-08-06 17:24:21 +0200

    added more components

--- 8. redux_skype_simple/src/Containers/App.js
+++ 8. redux_skype_simple/src/Containers/App.js
@@ -5,11 +5,11 @@ import Main from "../components/Main";
 import store from "../store";
 
 function App() {
-  const { contacts } = store.getState();
+  const { contacts, user, activeUserId } = store.getState();
   return (
     <div className="App">
       <Sidebar contacts={_.values(contacts)} />
-      <Main />
+      <Main user={user} activeUserId={activeUserId} />
     </div>
   );
 }
--- 8. redux_skype_simple/src/components/Main.js
+++ 8. redux_skype_simple/src/components/Main.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Main.css";
-
-const Main = () => {
-  return <main className="Main">Main Stuff</main>;
+import Empty from "../components/Empty";
+import ChatWindow from "../components/ChatWindow";
+const Main = ({ user, activeUserId }) => {
+  const renderMainContent = () => {
+    if (!activeUserId) {
+      return <Empty user={user} activeUserId={activeUserId} />;
+    } else {
+      return <ChatWindow activeUserId={activeUserId} />;
+    }
+  };
+  return <main className="Main">{renderMainContent()}</main>;
 };
-
 export default Main;
