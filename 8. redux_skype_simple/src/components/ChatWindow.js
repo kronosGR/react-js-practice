bf8c395f0e2a39fef5afa95046fb6b2020f11312
kronosGR <geo.elgeo@gmail.com>
2022-08-07 16:32:36 +0200

    added chats and chatwindow component

--- 8. redux_skype_simple/src/components/ChatWindow.js
+++ 8. redux_skype_simple/src/components/ChatWindow.js
@@ -1,13 +1,18 @@
 import React from "react";
+import _ from "lodash";
+
 import store from "../store";
 import Header from "./Header";
+import Chats from "./Chats";
 
 const ChatWindow = ({ activeUserId }) => {
   const state = store.getState();
+  const activeMsgs = state.messages[activeUserId];
   const activeUser = state.contacts[activeUserId];
   return (
     <div className="ChatWindow">
       <Header user={activeUser} />
+      <Chats messages={_.values(activeMsgs)} />
     </div>
   );
 };
