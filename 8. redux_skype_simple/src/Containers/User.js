7e9d7e9693b2e7d24abb8612645ffdc261aa09e7
kronosGR <geo.elgeo@gmail.com>
2022-08-07 16:22:55 +0200

    added actions and constants

--- 8. redux_skype_simple/src/Containers/User.js
+++ 8. redux_skype_simple/src/Containers/User.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./User.css";
+import store from "../store";
+import { setActiveUserId } from "../action";
 const User = ({ user }) => {
   const { name, profile_pic, status } = user;
+
+  const handleUserClick = ({ user_id }) => {
+    store.dispatch(setActiveUserId(user_id));
+  };
   return (
-    <div className="User">
+    <div className="User" onClick={() => handleUserClick(user)}>
       <img src={profile_pic} alt={name} className="User__pic" />
       <div className="User__details">
         <p className="User__details-name">{name}</p>
